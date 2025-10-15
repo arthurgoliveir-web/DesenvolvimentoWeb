94d42d4600ac4f6d059465b594260fbeedf461a4
BernardoCastroo <bernardofdecastro@gmail.com>
2025-10-15 14:08:39 -0300

    Update script.js

--- PI - Web/script.js
+++ PI - Web/script.js
@@ -16,4 +16,35 @@ function mostrarCategoria(id)
             li.classList.add('ativo');
         }
     })
+
 }
+// validação de email e constant 
+const emailInput = document.getElementById("email");
+const hintEmail = document.getElementById("hint-email");
+
+const reMail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;// validar e-mail e dominio
+const vMail = v => reMail.test(v.trim());
+
+function validarEmail() {
+    const valor = emailInput.value;
+    if (!vMail(valor)) {
+        hintEmail.textContent = "Digite um e-mail válido (exemplo@dominio.com)";
+        hintEmail.style.color = "red";
+        return false;
+    } else {
+        hintEmail.textContent = "";
+        return true;
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    if (form) {
+        form.addEventListener("submit", function(e) {
+            if (!validarEmail()) {
+                e.preventDefault();
+                emailInput.focus();
+            }
+        });
+        emailInput.addEventListener("input", validarEmail);
+    }
+});
